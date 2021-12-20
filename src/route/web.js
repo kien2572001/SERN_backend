3f3ab996440bad8ac3bd03199de9bc5c396bacdb
kien2572001 <kien2572001.ntk@gmail.com>
2021-12-20 21:31:06 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,17 @@
+import express from "express";
+import homeController from '../controllers/homeController'
+
+let router = express.Router()
+
+let initWebRoutes = (app)=>{
+    router.get('/',(req,res)=>{
+        return res.send('Hello world')
+    })
+
+    router.get('/home',homeController.getHomePage)
+
+    return app.use("/",router)
+
+}
+
+module.exports = initWebRoutes
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+require('dotenv').config();
+
+
+const app = express()
+
+//Config
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+viewEngine(app)
+initWebRoutes(app)
+
+let port = process.env.PORT || 6969 //Neu ko ton tai thi se la 6969
+
+app.listen(port,()=>{
+    console.log("Backend nodejs is running on PORT: "+ port)
+})
