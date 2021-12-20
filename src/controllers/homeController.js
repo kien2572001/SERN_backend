25a96deb81cc1b00a51192c0cfb2c72eca7fdd1b
kien2572001 <kien2572001.ntk@gmail.com>
2021-12-21 00:29:37 +0700

    xong vide #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,17 @@
-let getHomePage = (req,res)=>{
-    return res.render('homepage',{})
+import db from '../models/index'
+
+let getHomePage = async (req,res)=>{
+    try {
+        let data = await db.User.findAll()
+        console.log(JSON.stringify(data))
+        return res.render('homepage',{
+            data: JSON.stringify(data)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    
+    
 }
 module.exports = {
     getHomePage: getHomePage
